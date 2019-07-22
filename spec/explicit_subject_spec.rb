1594b60068f58e4f04ac9ad3ecfa8586700ca91c
dustinxie <dustinxie@hotmail.com>
2019-07-21 19:24:33 -0700

    explicit subject

---
+++ spec/explicit_subject_spec.rb
@@ -0,0 +1,16 @@
+RSpec.describe Hash do 
+    subject(:bob) do 
+        {a:1,b:2}
+    end
+    # let(:bob) {{a:1,b:2}}
+    it 'has two key-value pairs'do 
+        expect(subject.length).to eq(2)
+        expect(bob.length).to eq(2)
+    end
+    describe 'nested example'do 
+        it 'has two key-value pairs'do 
+        expect(subject.lentgh).to eq(2)
+        expect(bob.length).to eq(2s)
+        end 
+    end 
+end
